73072f0fb6a50895f2e46d3cf2e5acbf077bcae0
ismaelash <contato@ismaelnascimento.com>
2021-07-22 20:55:58 -0300

    [UPDATED] App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - with Expert Club
         </a>
       </header>
     </div>
