c4596654c1b5a11946d0069c80e8f83ef89a2f27
ismaelash <contato@ismaelnascimento.com>
2021-07-22 21:09:59 -0300

    [UPDATED] App.js, with instructor name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - with Expert Club
+          Learn React - with Expert Club (Ismael Ash)
         </a>
       </header>
     </div>
